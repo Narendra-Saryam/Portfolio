e4869230d6ed9da6efd792b5075304fe58802d99
nischay59 <narendrasaryam9459@gmail.com>
2025-04-03 23:38:35 +0530

    changes on home

--- src/Componentes/Home/Home.jsx
+++ src/Componentes/Home/Home.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import image from '/src/assets/n1.jpg'
+import image from '/src/assets/image/n1nobg.png'
 import TextChanger from '../Projects/TextChanger'
 
 const Home = () => {
   return (
-    <div id='Home' className='text-white flex w-full justify-between items-start p-20 md:p-20'>
+    <div id='Home' className='text-white flex flex-wrap-reverse w-full justify-between items-center p-10 md:p-20'>
         <div className='md:w-2/4 md:pt-10'>
             <h1 className='text-3xl md:text-6xl font-bold leading-normal tracking-tighter'>
                 {/* <TextChanger /> */}
                 Hello' I am Narendra
             </h1>
-            <p className='text-sm md:text-2xl tracking-tight'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque illum, natus non voluptatibus.
+            <p className='md:mt-5 text-sm md:text-2xl tracking-tight'>
+                I am a MERN Stack Developer, This is my Portfolio it contain all the information about my past activities and knowledge i gain.
             </p>
-            <button className='mt-5 md:mt-10 text-white py-2 px-3 text-sm md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Contact Me</button>
+            <button className='mt-5 md:mt-10 text-white py-2 px-3 text-sm md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'><a href="#Footer">Contact Me</a></button>
         </div>
-        <div><img className='h-96' src={image} alt="Hero Image" /></div>
+        <div className='p-20'>
+          <img className=' md:h-96 rounded-full bg-gradient-to-tl from-black via-[#171d32] shadow-xl mt-4 mr-10 hover:scale-110 duration-200' src={image} alt="Hero Image" />
+        </div>
     </div>
   )
 }
